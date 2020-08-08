46e61578ddfd72d54f93751af710106305bcf92a
CalvinE <calvin.echols@gmail.com>
2020-08-08 13:00:29 -0400

    working image import

---
+++ src/static_assets.d.ts
@@ -0,0 +1,5 @@
+// These modile declariations are to supress import errors when importing static 
+// assets like images and other stuff typescript normally does not let you import...
+
+declare module '*.png'
+declare module '*.PNG'
--- src/test.tsx
+++ src/test.tsx
@@ -1,7 +1,11 @@
 import {JSXFactory} from './jsx_renderer';
+import testImage from '../public/assets/test_img.PNG';
+
+const text = "It Works!";
 
 document.getElementById("app")?.appendChild(
     <div>
-        <h1>It Works!</h1>
+        <h1 id="textid">{text}</h1>
+        <img src={testImage}/>
     </div>
 );
